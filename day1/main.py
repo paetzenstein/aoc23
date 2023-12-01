a14369d7d0591ab3a23b4be1e049a4a0deb012a5
paetzenstein <paetzenstein@mailbox.org>
2023-12-01 14:12:01 +0100

    complete challenge

--- day1/main.py
+++ day1/main.py
@@ -13,17 +13,53 @@ def read_file(filename):
         return None
 
 
-def create_output_file(filename, output):
-    try:
-        with open(filename, 'w') as file:
-            for lines in output:
-                file.write(lines)
-    except Exception as error:
-        print(f"An unexpected error occurred: {error}")
+def get_calibration_value(current_line):
+    calibration_list = []
+    reversed_line = "".join(current_line[::-1])
+    calibration_numbers = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    reversed_calibration_numbers = ("eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin")
+    # Getting first number
+    for char in current_line:
+        counter = 1
+        if char.isdigit():
+            calibration_list.append(char)
+            break
+        if current_line.startswith(calibration_numbers):
+            for num in calibration_numbers:
+                if current_line.startswith(num):
+                    calibration_list.append(str(calibration_numbers.index(num) + 1))
+                    break
+            break
+        else:
+            current_line = current_line[counter:]
+            counter += 1
+    # Getting second number
+    for char in reversed_line:
+        counter = 1
+        if char.isdigit():
+            calibration_list.append(char)
+            break
+        if reversed_line.startswith(reversed_calibration_numbers):
+            for num in reversed_calibration_numbers:
+                if reversed_line.startswith(num):
+                    calibration_list.append(str(reversed_calibration_numbers.index(num) + 1))
+                    break
+            break
+        else:
+            reversed_line = reversed_line[counter:]
+            counter += 1
+    # Return result as int
+    return int(calibration_list[0] + calibration_list[1])
 
 
 def main():
-    pass
+    filename = "input.txt"
+    puzzle_input = read_file(filename)
+    sum_of_calibration_values = 0
+    for current_line in puzzle_input:
+        calibration_values = get_calibration_value(current_line)
+        sum_of_calibration_values += calibration_values
+    print(sum_of_calibration_values)
 
 
 if __name__ == '__main__':
